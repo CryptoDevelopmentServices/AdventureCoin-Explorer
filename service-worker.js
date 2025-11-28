bbef3d22d4dd90a26423e669d2e31bb364cf56be
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2025-11-28 16:54:02 +0000

    test

--- service-worker.js
+++ service-worker.js
@@ -6,6 +6,15 @@ const assets = [
   '/main.js',
   '/img/favicon-192.png',
   '/img/favicon-512.png',
+  '/wallet/index.html',
+  '/wallet/multilang.js',
+  '/wallet/bitcoinJS-lib.js',
+  '/paper-wallets/walletgen.html',
+  '/paper-wallets/wallets/sample1.png',
+  '/paper-wallets/wallets/sample2.png',
+  '/paper-wallets/wallets/sample3.png',
+  '/paper-wallets/wallets/sample4.png',
+  '/paper-wallets/wallets/sample5.png',
   // Add other files to cache as needed
 ];
 
